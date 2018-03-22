d823d90c47e9cbfcb3b61a17239da723a07080ea
daltobon <daniel18.tobon@gmail.com>
2018-03-21 19:07:17 -0500

    Vistas show y edit para overflow

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,16 +1,48 @@
 class QuestionsController < ApplicationController
   def index
+    @questions = Question.all
   end
 
   def show
+    @question = Question.find(params[:id])
   end
 
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.create(questions_params)
+    if @question.save
+      redirect_to question_path(@question), notice: "Pregunta publicada exitosamente"
+    else
+      render :edit      
+    end
+  end
+  
   def edit
+     @question = Question.find(params[:id])
+  end
+
+  def update
+    @question = question.find(params[:id])
+    if @question.update(question_params)
+      redirect_to questions_path, notice: "Tu pregunta ha sido actualizada"
+    else
+      render :edit, notice: "No se pudo actualizar el registro, intente de nuevo"
+    end
   end
 
   def delete
   end
 
-  def new
+  def destroy
+    
   end
+
+  private
+      def questions_params
+        params.require(:question).permit(:title, :description, :vote, :id)
+    end
+
 end
